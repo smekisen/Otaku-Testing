d42423a21bae8e8dd8f63df59c314e3d722362f6
Goldenfreddy0703 <62456796+Goldenfreddy0703@users.noreply.github.com>
2024-10-22 15:24:10 -0400

    Updated Version v5.1.2
    Changelog v5.1.2 (2024-10-22):
    - Fixed Clearlogos

--- plugin.video.otaku/resources/lib/indexers/fanart.py
+++ plugin.video.otaku/resources/lib/indexers/fanart.py
@@ -13,66 +13,54 @@ def getArt(meta_ids, mtype):
         if res:
             if mtype == 'movies':
                 if res.get('moviebackground'):
-                    items = []
-                    for item in res['moviebackground']:
-                        if item.get('lang') in lang:
-                            items.append(item.get('url'))
+                    items = [item.get('url') for item in res['moviebackground'] if item.get('lang') in lang]
                     art['fanart'] = items
                 if res.get('moviethumb'):
-                    items = []
-                    for item in res['moviethumb']:
-                        if item.get('lang') in lang:
-                            items.append(item.get('url'))
+                    items = [item.get('url') for item in res['moviethumb'] if item.get('lang') in lang]
                     art['thumb'] = items
             else:
                 if res.get('showbackground'):
-                    items = []
-                    for item in res['showbackground']:
-                        if item.get('lang') in lang:
-                            items.append(item.get('url'))
+                    items = [item.get('url') for item in res['showbackground'] if item.get('lang') in lang]
                     art['fanart'] = items
                 if res.get('tvthumb'):
-                    items = []
-                    for item in res['tvthumb']:
-                        if item.get('lang') in lang:
-                            items.append(item.get('url'))
+                    items = [item.get('url') for item in res['tvthumb'] if item.get('lang') in lang]
                     art['thumb'] = items
 
             if res.get('clearart'):
-                items = []
-                for item in res['clearart']:
-                    if item.get('lang') in lang:
-                        items.append(item.get('url'))
+                items = [item.get('url') for item in res['clearart'] if item.get('lang') in lang]
                 art['clearart'] = items
             elif res.get('hdclearart'):
-                items = []
-                for item in res['hdclearart']:
-                    if item.get('lang') in lang:
-                        items.append(item.get('url'))
+                items = [item.get('url') for item in res['hdclearart'] if item.get('lang') in lang]
                 art['clearart'] = items
             elif res.get('hdmovieclearart'):
-                items = []
-                for item in res['hdmovieclearart']:
-                    if item.get('lang') in lang:
-                        items.append(item.get('url'))
+                items = [item.get('url') for item in res['hdmovieclearart'] if item.get('lang') in lang]
                 art['clearart'] = items
 
             if res.get('clearlogo'):
-                items = []
-                for item in res['clearlogo']:
-                    if item.get('lang') in lang:
-                        items.append(item.get('url'))
-                art['clearlogo'] = items
+                items = sorted([item for item in res['clearlogo'] if item.get('lang') in lang], key=lambda x: int(x['id']))
+                logos = []
+                try:
+                    logos.append(next(x['url'] for x in items if x['lang'] == 'en'))
+                except StopIteration:
+                    pass
+                try:
+                    logos.append(next(x['url'] for x in items if x['lang'] == 'ja'))
+                except StopIteration:
+                    pass
+                art['clearlogo'] = logos
             elif res.get('hdtvlogo'):
-                items = []
-                for item in res['hdtvlogo']:
-                    if item.get('lang') in lang:
-                        items.append(item.get('url'))
-                art['clearlogo'] = items
+                items = sorted([item for item in res['hdtvlogo'] if item.get('lang') in lang], key=lambda x: int(x['id']))
+                logos = []
+                try:
+                    logos.append(next(x['url'] for x in items if x['lang'] == 'en'))
+                except StopIteration:
+                    pass
+                try:
+                    logos.append(next(x['url'] for x in items if x['lang'] == 'ja'))
+                except StopIteration:
+                    pass
+                art['clearlogo'] = logos
             elif res.get('hdmovielogo'):
-                items = []
-                for item in res['hdmovielogo']:
-                    if item.get('lang') in lang:
-                        items.append(item.get('url'))
+                items = [item.get('url') for item in res['hdmovielogo'] if item.get('lang') in lang]
                 art['clearlogo'] = items
     return art
